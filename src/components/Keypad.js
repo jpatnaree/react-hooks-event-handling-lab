0465e0ef6b8cd8e29ed3081202d9d4425b0618d9
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-10-25 15:40:26 -0400

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function handleOnChange() {
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <div>
+            <input onChange={handleOnChange} type="password"/>
+        </div>
     )
 }
 
